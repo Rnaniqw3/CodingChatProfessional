5a2d054a6590483268ced2e8e680602183c59aa3
Rnaniqw3 <95932621+Rnaniqw3@users.noreply.github.com>
2021-12-10 16:26:30 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,76 @@
+mkdir ccp
+cd ccp
+git clone https://Rnaniqw3.github.com/CodingChatProfessional/index.html
+cat index.html
+echo "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"
+echo "Head to bit.ly/codingchatpro to activate your membership."
